ecf5a619aedca720a46ea1f32f3c4e88237cd083
Domenique <domogenesis77@gmail.com>
2023-05-30 01:45:04 -0500

    submission

--- test/indexTest.js
+++ test/indexTest.js
@@ -21,7 +21,7 @@ describe('logShout(string)', function() {
 
     expect(spy).toHaveBeenCalledWith('HELLO');
 
-    console.log.restore();
+    console.log.restore ();     
   })
 })
 
@@ -52,3 +52,31 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
     );
   });
 })
+
+
+function shout(string) {
+  return string.toUpperCase();}
+
+  function whisper(string) {
+    return string.toLowerCase();}
+
+  function logShout (string) {
+    console.log(string.toUpperCase( ));
+  }
+
+  function logWhisper (string) {
+    console.log(string.toLowerCase( ));
+  }
+
+  function sayHiToHeadphonedRoommate(string){ 
+    if (string.toLowerCase(string) === string) 
+  
+    {return "I can\'t hear you!" ;}
+  
+    if (string.toUpperCase(string) === string) 
+    
+    { return "YES INDEED!" ;}
+  
+    if ("Let\'s have dinner together!" === string) 
+    {
+    return "I would love to!" ;};}
